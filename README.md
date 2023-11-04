4b692f1ea8330d1c902419d5e381146477f7afb2
abols360 <aleksis.abols@va.lv>
2023-11-05 00:31:50 +0200

    Fix image adding, create program restart and playagin button

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Useful links: 
 
-https://www.google.com/search?q=tic+tac+to+with+custom+images+android+studio&sca_esv=579423212&rlz=1C1GCEA_enLV929LV929&sxsrf=AM9HkKkghNa3RjsSqI53CrqvfEzxl-Lwgw%3A1699095360814&ei=QCNGZaefMd6Jxc8P3PuO6Ac&udm=&oq=tic+tac+to+with+custom+images+android+st&gs_lp=Egxnd3Mtd2l6LXNlcnAiKHRpYyB0YWMgdG8gd2l0aCBjdXN0b20gaW1hZ2VzIGFuZHJvaWQgc3QqAggAMgcQIRigARgKSOMjUKYEWO4ecAN4AJABAJgBzwGgAd0JqgEFMC44LjG4AQPIAQD4AQHCAggQIRgWGB4YHcICBBAhGBXiAwQYASBBiAYB&sclient=gws-wiz-serp#fpstate=ive&vld=cid:1e0b41a4,vid:uri1QHolQT8,st:0
+https://www.youtube.com/watch?v=uri1QHolQT8&ab_channel=BrainOnCode
 
 https://www.youtube.com/watch?v=NyUbptjoqb8&ab_channel=AllCodingTutorials
 
--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 
 import android.os.Bundle;
@@ -10,302 +11,173 @@ import android.view.View;
 
 import android.widget.Button;
 
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
     boolean playerOneActive;
-
     private TextView playerOneScore, playerTwoScore, playerStatus;
-
-    private Button[] buttons = new Button[9];
-
+    private ImageButton[] buttons = new ImageButton[9];
     private Button reset, playagain;
-
+    boolean isButtonPressed = false;
     int[] gameState = {2,2,2,2,2,2,2,2,2};
-
     int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6},
-
             {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
-
     int rounds;
 
+    TextView textView;
+    Button resetButton;
     private int playerOneScoreCount, playerTwoScoreCount;
-
     @Override
-
-    protected void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-
-
         playerOneScore = findViewById(R.id.score_Player1);
-
         playerTwoScore = findViewById(R.id.score_Player2);
-
         playerStatus = findViewById(R.id.textStatus);
-
         reset = findViewById(R.id.btn_reset);
-
         playagain = findViewById(R.id.btn_play_again);
-
-
-
         buttons[0] = findViewById(R.id.btn0);
-
         buttons[1] = findViewById(R.id.btn1);
-
         buttons[2] = findViewById(R.id.btn2);
-
         buttons[3] = findViewById(R.id.btn3);
-
         buttons[4] = findViewById(R.id.btn4);
-
         buttons[5] = findViewById(R.id.btn5);
-
         buttons[6] = findViewById(R.id.btn6);
-
         buttons[7] = findViewById(R.id.btn7);
-
         buttons[8] = findViewById(R.id.btn8);
-
-
-
         for(int i=0; i<buttons.length; i++)
-
         {
-
             buttons[i].setOnClickListener(this);
-
         }
-
         playerOneScoreCount = 0;
-
         playerTwoScoreCount = 0;
-
         playerOneActive = true;
-
         rounds = 0;
-
     }
 
-
     @Override
-
     public void onClick(View view)
-
     {
-
-        if(!((Button)view).getText().toString().equals(""))
-
+//        if(view.drawable != null)
+//        {
+//            return;
+//        }
+        if(checkWinner())
         {
-
             return;
-
         }
-
-        else if(checkWinner())
-
-        {
-
-            return;
-
-        }
-
         String buttonID  = view.getResources().getResourceEntryName(view.getId());
-
         int gameStatePointer = Integer.parseInt(buttonID.substring(buttonID.length()-1,buttonID.length()));
 
+//        ImageButton imageButton1 = findViewById(R.id.btn0);
+//        int currentImageResource = (int) imageButton1.getTag();
 
-
-        if(playerOneActive)
-
-        {
-
-//            ((Button)view).setText("X");
-            ((Button)view).setBackgroundResource(R.drawable.samsung);
-           // ((Button)view).setBackgroundColor(Color.parseColor("#000000"));
-
-           // ((Button)view).set;
-
-            //((Button)view).setTextColor(Color.parseColor("#ffc34a"));
-
-            gameState[gameStatePointer] = 0;
-
-        }
-
-        else
-
-        {
-
-            //((Button)view).setText("O");
-
-            //((Button)view).setText("O");
-            ((Button)view).setBackgroundResource(R.drawable.apple);
-            //((Button)view).setColo
-           // ((Button)view).setBackgroundColor(Color.parseColor("#000000"));
-            //((Button)view).set(R.drawable.apple);
-            //((Button)view).setTextColor(Color.parseColor("#70fc3a"));
-
-            gameState[gameStatePointer] = 1;
-
-        }
-
-        rounds++;
-
-
+//        if (!isButtonPressed);
+//        {
+            if(playerOneActive)
+            {
+                ((ImageButton)view).setBackgroundResource(R.drawable.samsung);
+                gameState[gameStatePointer] = 0;
+            }
+            else
+            {
+                ((ImageButton)view).setBackgroundResource(R.drawable.apple);
+                gameState[gameStatePointer] = 1;
+            }
+            rounds++;
+            isButtonPressed = true;
+//        }
 
         if(checkWinner())
-
         {
-
             if(playerOneActive)
-
             {
-
                 playerOneScoreCount++;
-
                 updatePlayerScore();
-
-                playerStatus.setText("Player-1 has won");
-
-            }else
-
+                playerStatus.setText("Samsung has won");
+            }
+            else
             {
-
                 playerTwoScoreCount++;
-
                 updatePlayerScore();
-
-                playerStatus.setText("Player-2 has won");
-
+                playerStatus.setText("Apple has won");
             }
-
         }
-
         else if(rounds==9)
-
         {
-
             playerStatus.setText("No Winner");
-
         }
-
         else
-
         {
-
             playerOneActive = !playerOneActive;
-
         }
 
-
-
-        reset.setOnClickListener(new View.OnClickListener() {
-
+        reset.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-
-            public void onClick(View view) {
-
+            public void onClick(View view)
+            {
                 playAgain();
-
                 playerOneScoreCount= 0;
-
                 playerTwoScoreCount= 0;
-
                 updatePlayerScore();
-
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
             }
-
         });
 
-
-
         playagain.setOnClickListener(new View.OnClickListener() {
-
             @Override
-
             public void onClick(View view) {
-
+                //((ImageButton)view).setBackgroundResource(R.drawable.custom_button);
                 playAgain();
-
+             //   ((ImageButton)view).setBackgroundResource(R.drawable.custom_button);
+                for(int i=0; i<buttons.length; i++)
+                {
+                    buttons[i].setBackgroundResource(R.drawable.custom_button);
+                }
             }
         });
     }
 
     private boolean checkWinner()
     {
-
         boolean winnerResults  = false;
-
         for (int[] winningPositions: winningPositions)
-
         {
-
             if(gameState[winningPositions[0]]==gameState[winningPositions[1]]&&
-
                     gameState[winningPositions[1]]==gameState[winningPositions[2]] &&
-
                     gameState[winningPositions[0]]!=2)
-
             {
-
                 winnerResults = true;
-
             }
-
         }
-
-
-
         return winnerResults;
-
-
-
     }
 
-
-
     private void playAgain()
-
     {
-
         rounds = 0;
-
         playerOneActive = true;
-
         for (int i=0; i<buttons.length; i++)
-
         {
-
             gameState[i] = 2;
-
-            buttons[i].setText("");
-
         }
-
         playerStatus.setText("Status");
 
 
-
     }
-
-
 
     private void updatePlayerScore()
-
     {
-
         playerOneScore.setText(Integer.toString(playerOneScoreCount));
-
         playerTwoScore.setText(Integer.toString(playerTwoScoreCount));
-
     }
-
 }
